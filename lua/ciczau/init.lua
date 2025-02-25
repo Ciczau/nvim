279b058e8baf7c98ac4c356eeb7aa73c76d88f23
Ciczau <wiktor.michalski@outlook.com>
2025-02-25 15:05:24 +0100

    chore: prevent copy to clipboard when deleting

--- lua/ciczau/init.lua
+++ lua/ciczau/init.lua
@@ -61,6 +61,3 @@ autocmd('LspAttach', {
 })
 
 vim.opt.clipboard = "unnamedplus"
-vim.g.netrw_browse_split = 0
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
--- lua/ciczau/remap.lua
+++ lua/ciczau/remap.lua
@@ -27,7 +27,8 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+vim.keymap.set({ "n", "v" }, "d", "\"_d")
+vim.keymap.set({ "n", "v" }, "dd", "\"_dd")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
