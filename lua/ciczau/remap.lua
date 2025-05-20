4714425b1657ab3a9a771575c73f97b98e8132e6
Ciczau <wiktor.michalski@outlook.com>
2025-05-20 16:59:48 +0200

    fix: proper move up/down

--- lua/ciczau/remap.lua
+++ lua/ciczau/remap.lua
@@ -3,7 +3,9 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "K", ":m .-2<CR>==", { noremap = true, silent = true })
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
